7555fa349e72e18671cd5e16ae3c7727c63c3178
mort <manuel@simplelogica.net>
2009-06-19 01:45:35 +0200

    Setting up specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,20 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/autorun'
 require 'spec/rails'
+require File.expand_path(File.dirname(__FILE__) + "/blueprints")
+
 
 Spec::Runner.configure do |config|
+  config.mock_with :rr
+  config.before(:each) { Sham.reset }
+  
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
-  config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  #config.use_transactional_fixtures = true
+  #config.use_instantiated_fixtures  = false
+  #config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  
 
   # == Fixtures
   #
@@ -39,7 +45,6 @@ Spec::Runner.configure do |config|
   #
   # config.mock_with :mocha
   # config.mock_with :flexmock
-  # config.mock_with :rr
   #
   # == Notes
   # 
