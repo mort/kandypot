36d8cfe7933d03da6399c4073d939b12bdf02ec2
mort <manuel@simplelogica.net>
2009-06-19 01:43:03 +0200

    Machinist gem

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem 'wvanbergen-http_status_exceptions', :lib => 'http_status_exceptions', :source => 'http://gems.github.com'
   config.gem 'logging'
   config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
+  config.gem 'notahat-machinist', :lib => 'machinist', :source => 'http://gems.github.com'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
