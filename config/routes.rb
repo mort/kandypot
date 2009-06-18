19ffdaddf5f5c571b6623aaa008acfdd92caf293
mort <manuel@simplelogica.net>
2009-06-18 19:40:21 +0200

    Renaming Account resource to App

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :accounts
+  map.resources :apps
 
   # The priority is based upon order of creation: first created -> highest priority.
 
---
+++ db/migrate/20090618172719_create_apps.rb
@@ -0,0 +1,36 @@
+class CreateApps < ActiveRecord::Migration
+  def self.up
+    create_table :apps do |t|
+
+      t.with_options :length => 100, :null => false do |c|
+        t.string :name 
+        t.string :nicename   
+        t.string :url
+        t.string :app_key
+        t.string :app_token
+      end
+
+      t.string :ip, :limit => 15
+
+      t.text :description
+
+      t.integer :members_count, :default => 0, :null => false
+      t.integer :kandies_count, :default => 0, :null => false
+
+      t.integer :status, :limit => 2, :default => 1, :null => false
+
+      t.timestamps
+    end
+
+    add_index(:apps, :name, :unique => true)    
+    add_index(:apps, :url, :unique => true)    
+    add_index(:apps, :app_key, :unique => true)    
+    add_index(:apps, :app_token, :unique => true)    
+    add_index(:apps, :ip, :unique => true)    
+    
+  end
+
+  def self.down
+    drop_table :apps
+  end
+end
