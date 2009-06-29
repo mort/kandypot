bc6583ddb3a0b8a456fd4c9c9753817910f40ae0
mort <manuel@simplelogica.net>
2009-06-29 11:17:36 +0200

    Getting member stats

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,25 @@
 class MembersController < ApplicationController
+  before_filter :require_app
+  
+  def show
+    @member = @app.members.find_by_member_token(params[:id])
+    if @member
+      render :json => {:member_token => @member.member_token, :kandies_count => @member.kandies_count}, :status => :ok
+    else
+      render :text => '', :status => :not_found  
+    end
+  end
+  
+  private
+  
+  def require_app
+    @app = App.find_by_id params[:app_id]
+    unless @app
+      render :text => '', :status => :not_found 
+    else
+      str = Digest::SHA1.hexdigest("#{params[:id]}###{Time.now.midnight.to_s}")    
+      render :text => '', :status => :forbidden unless App.authenticate(@app.app_token, str, params[:signature])
+    end  
+  end
+
 end
---
+++ spec/controllers/members_spec.rb
@@ -0,0 +1,50 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe MembersController, 'get' do
+  before do
+    @member = Member.make
+  end
+  
+  it 'should respond with not found to a bad app id' do
+    get :show, :id => @member.member_token, :app_id => 'foo'
+    response.response_code.should == 404
+  end
+
+  it 'should respond with forbidden to no signature' do
+    get :show, :id => @member.member_token, :app_id => @member.app.id
+    response.response_code.should == 403
+  end  
+  
+end
+
+describe MembersController, 'a correct request' do
+  before do
+    @member = Member.make(:kandies_count => 20)
+    str =  Digest::SHA1.hexdigest("#{@member.member_token}###{Time.now.midnight.to_s}")
+    signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, @member.app.app_key, str)
+    get :show, :id => @member.member_token, :app_id => @member.app.id, :signature => signature
+  end
+  
+  it 'should respond with success' do  
+    response.response_code.should == 200
+  end
+  
+  it 'should respond with a jsonified hash' do
+    ActiveSupport::JSON::decode(response.body).class.should == Hash
+  end
+  
+  it 'should have to elements' do
+    ActiveSupport::JSON::decode(response.body).keys.size.should == 2
+  end
+  
+  it 'should carry the kandies count' do
+    ActiveSupport::JSON::decode(response.body)['kandies_count'].should == 20
+  end
+  
+  it 'should carry the member token' do
+    ActiveSupport::JSON::decode(response.body)['member_token'].should == @member.member_token
+  end
+  
+  
+  
+end
