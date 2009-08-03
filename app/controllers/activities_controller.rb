8b012bd26511a677947b8f89764b148e399ca04d
mort <manuel@simplelogica.net>
2009-08-03 13:20:07 +0200

    Setting IP for the activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,8 @@ class ActivitiesController < ApplicationController
   def create
     params.delete_if{|k,v| !Activity.column_names.include?(k) }
     @activity = @app.activities.build(params)
+    @activity.ip =  request.env['HTTP_X_FORWARDED_FOR'] || request.env['HTTP_CLIENT_IP'] || request.env['REMOTE_ADDR']))
+    
     if @activity.save
       render :json => @activity.to_json, :status => :created 
     elsif @activity.errors.on(:authentication)
