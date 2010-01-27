b54d3f0bd14a6081e6962df03f5875563abd8fbc
mort <manuel@simplelogica.net>
2010-01-27 16:57:39 +0100

    Fuera hoptad/capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,4 +88,4 @@ Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-
   $: << File.join(vendored_notifier, 'lib')
 end
 
-require 'hoptoad_notifier/capistrano'
+#require 'hoptoad_notifier/capistrano'
