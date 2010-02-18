c664de9ace3b02cea8c57d485d5fd35788a5b9cc
mort <manuel@simplelogica.net>
2010-02-19 00:50:56 +0100

    Deactivate Hoptoad

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   config.gem 'fastercsv'
   config.gem 'daemons'
   config.gem "mholling-subdomain_routes", :lib => "subdomain_routes", :source => "http://gems.github.com"
-  config.gem 'hoptoad_notifier'
+  #config.gem 'hoptoad_notifier'
   #config.gem "tobi-delayed_job", :lib => "delayed_job", :source => "http://gems.github.com"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
